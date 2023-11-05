8ebb2cfc6707c6758a1862a30ed991f9c0043e47
shuaibq3 <shuaib_q3@yahoo.com>
2023-11-05 16:01:57 +0600

    I-#4 feedback adoption - ts-jest is used instead of babel-jest in jest.config - engines specified to >=18 in package.json - type specified as module in package.json

--- jest.config.ts
+++ jest.config.ts
@@ -179,7 +179,7 @@ const config: Config = {
 
   // A map from regular expressions to paths to transformers
   transform: {
-    '^.+\\.ts?$': 'babel-jest'
+    '^.+\\.ts?$': 'ts-jest'
   }
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
