fea70860088742f888b304b78bf5a7b015194b49
Melon <melon.ruet@gmail.com>
2024-03-18 15:50:55 +0600

    I-#17: api gateway url output

--- deployment/localstack/terraform/outputs.tf
+++ deployment/localstack/terraform/outputs.tf
@@ -1,3 +1,4 @@
 output "aws_invoke_base_url" {
-  value = module.aws.invoke_base_url
+  // source: https://docs.localstack.cloud/user-guide/aws/apigateway/
+  value = "http://${module.aws.rest_api_id}.execute-api.localhost.localstack.cloud:4566/${module.aws.api_deployment_stage_name}"
 }
--- iac/terraform/aws/outputs.tf
+++ iac/terraform/aws/outputs.tf
@@ -1,3 +1,11 @@
 output "invoke_base_url" {
   value = aws_api_gateway_deployment.api_deployment.invoke_url
 }
+
+output "rest_api_id" {
+  value = aws_api_gateway_rest_api.rest_api.id
+}
+
+output "api_deployment_stage_name" {
+  value = aws_api_gateway_deployment.api_deployment.stage_name
+}
