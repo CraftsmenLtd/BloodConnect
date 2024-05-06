e7b1a5fab504c6af94a1335bcf5833bde9a37329
Melon <melon.ruet@gmail.com>
2024-05-06 14:55:21 +0600

    I-#32: include api gateway deployment

--- iac/terraform/aws/api_gw.tf
+++ iac/terraform/aws/api_gw.tf
@@ -11,6 +11,19 @@ resource "aws_api_gateway_rest_api" "rest_api" {
   }
 }
 
+resource "aws_api_gateway_deployment" "api_deployment" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  stage_name  = var.environment
+
+  depends_on = [
+    module.auth
+  ]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
 #resource "aws_api_gateway_domain_name" "api_gw_domain_name" {
 #  domain_name     = "${var.environment}.${var.bloodconnect_domain}"
 #}
