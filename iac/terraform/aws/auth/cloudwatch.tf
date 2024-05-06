6f37da0db4e4cc74f20e147a086bd2afa2932a9f
Melon <melon.ruet@gmail.com>
2024-05-06 15:18:11 +0600

    I-#32: checkov for cloudwatch fix

--- iac/terraform/aws/auth/cloudwatch.tf
+++ iac/terraform/aws/auth/cloudwatch.tf
@@ -1,3 +1,6 @@
 resource "aws_cloudwatch_log_group" "apigateway_logs" {
+  #checkov:skip=CKV_AWS_158: "Ensure that CloudWatch Log Group is encrypted by KMS"
+  #checkov:skip=CKV_AWS_338: "Ensure CloudWatch log groups retains logs for at least 1 year"
   name = "${var.environment}-apigateway-logs"
+  retention_in_days = 30
 }
