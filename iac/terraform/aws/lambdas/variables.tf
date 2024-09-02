4d53f446694d32f15fdc174e5430a5f52051bd04
mahinuzzaman <mahinuzzaman.md@gmail.com>
2024-09-03 00:10:56 +0600

    I-#96: Added lambdas module in terraform

---
+++ iac/terraform/aws/lambdas/variables.tf
@@ -0,0 +1,33 @@
+variable "environment" {
+  type        = string
+  description = "Deployment environment"
+}
+
+variable "lambda_runtime" {
+  type        = string
+  description = "Lambda Runtime"
+  default     = "nodejs20.x"
+}
+
+variable "lambda_architecture" {
+  type        = list(string)
+  description = "Lambda ArchitectureInfo"
+  default     = ["arm64"]
+}
+
+variable "lambda_options" {
+  description = "Map of Lambda function configurations"
+  type = map(object({
+    name     = string
+    zip_path = string
+    handler  = string
+    statement = list(object({
+      sid       = string
+      actions   = list(string)
+      resources = list(string)
+    }))
+    env_variables = map(string)
+    timeout       = optional(number, 60)
+    memory_size   = optional(number, 128)
+  }))
+}
