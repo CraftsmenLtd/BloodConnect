b0057aeffe909111ba8d772ac0f195b14fd94284
biplob14 <biploburrahaman@gmail.com>
2024-09-03 19:50:37 +0600

    I-#74 checkov escape on localstack main route53

--- deployment/localstack/terraform/main.tf
+++ deployment/localstack/terraform/main.tf
@@ -4,6 +4,7 @@ module "aws" {
   hosted_zone_id = aws_route53_zone.my_zone.zone_id
 }
 
+#checkov:skip=CKV2_AWS_38: "Ensure Domain Name System Security Extensions (DNSSEC) signing is enabled for Amazon Route 53 public hosted zones"
 resource "aws_route53_zone" "my_zone" {
   name = "bloodconnect.net"
 }
