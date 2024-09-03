e3bef6490b8d71bca6a76370e1a23da15ece5dee
biplob14 <biploburrahaman@gmail.com>
2024-09-03 19:55:16 +0600

    I-#74 checkov escape line correction on localstack

--- deployment/localstack/terraform/main.tf
+++ deployment/localstack/terraform/main.tf
@@ -4,7 +4,6 @@ module "aws" {
   hosted_zone_id = aws_route53_zone.my_zone.zone_id
 }
 
-#checkov:skip=CKV2_AWS_38: "Ensure Domain Name System Security Extensions (DNSSEC) signing is enabled for Amazon Route 53 public hosted zones"
 resource "aws_route53_zone" "my_zone" {
   #checkov:skip=CKV2_AWS_38: "Ensure Domain Name System (DNS) query logging is enabled for Amazon Route 53 hosted zones"
   #checkov:skip=CKV2_AWS_39: "Ensure Domain Name System Security Extensions (DNSSEC) signing is enabled for Amazon Route 53 public hosted zones"
