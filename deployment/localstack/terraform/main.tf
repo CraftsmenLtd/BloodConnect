48acac675244a1747ae4e1decd2d9dddb4629960
biplob14 <biploburrahaman@gmail.com>
2024-09-03 19:29:13 +0600

    I-#74 ACM SSL certificate to route53 domain

--- deployment/localstack/terraform/main.tf
+++ deployment/localstack/terraform/main.tf
@@ -1,4 +1,9 @@
 module "aws" {
-  source              = "../../../iac/terraform/aws"
-  environment         = "localstack"
+  source         = "../../../iac/terraform/aws"
+  environment    = "localstack"
+  hosted_zone_id = aws_route53_zone.my_zone.zone_id
+}
+
+resource "aws_route53_zone" "my_zone" {
+  name = "bloodconnect.net"
 }
---
+++ iac/terraform/aws/route53/variables.tf
@@ -0,0 +1,17 @@
+variable "domain_name" {
+    default = "bloodconnect.net"
+    description = "bloodconnect domain name"
+    type = string
+}
+
+variable "domain_validation_method" {
+    default = "DNS"
+    description = "domain validation method"
+    type = string
+}
+
+variable "hosted_zone_id" {
+    default = "Z0872264215II0BIDZ5EC"
+    description = "hosted zone id for bloodconnect.net"
+    type = string
+}
