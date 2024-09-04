0765ca5ca4f1825d2c913d9ffce007972e97a44f
mahinuzzaman <mahinuzzaman.md@gmail.com>
2024-09-05 02:49:55 +0600

    I-#105: Added template file to update openapi place holders

--- iac/terraform/aws/scripts.tf
+++ iac/terraform/aws/scripts.tf
@@ -1,22 +1,35 @@
 resource "null_resource" "run_update_and_import_open_api_script" {
   provisioner "local-exec" {
-    environment = {
-      ACCOUNT_ID            = data.aws_caller_identity.current.account_id
-      AWS_REGION            = data.aws_region.current.name
-      ENVIRONMENT           = var.environment
-      API_GATEWAY_ID        = aws_api_gateway_rest_api.rest_api.id
-      OPENAPI_DIRECTORY     = var.openapi_directory
-      API_VERSION           = var.openapi_version
-      COMBINED_OPENAPI_FILE = var.combined_openapi_file
-    }
-    command = "${path.module}/scripts/import-openapi.sh"
+    command = "redocly bundle ${var.openapi_directory}/versions/${var.openapi_version}.yml -o ${var.combined_openapi_file}"
   }
-
-  depends_on = [
-    aws_api_gateway_rest_api.rest_api
-  ]
 
   triggers = {
     always_run = "${timestamp()}"
   }
+}
+
+locals {
+  all_lambda_invoke_arns = merge(
+    module.auth.lambda_invoke_arns
+  )
+}
+
+data "template_file" "openapi_definition" {
+  template = file(var.combined_openapi_file)
+
+  vars = merge({
+    ENVIRONMENT = var.environment
+    API_VERSION = var.openapi_version
+    },
+    local.all_lambda_invoke_arns
+  )
+
+  depends_on = [
+    null_resource.run_update_and_import_open_api_script
+  ]
+}
+
+resource "local_file" "openapi_output" {
+  filename = var.combined_openapi_file
+  content  = data.template_file.openapi_definition.rendered
 }
