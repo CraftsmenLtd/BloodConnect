8ffaa6e60e3d48dd9b1685bcb30d3c139506b14a
mahinuzzaman <mahinuzzaman.md@gmail.com>
2024-09-05 15:29:36 +0600

    I-#105: Removed unused variable from auth module

--- iac/terraform/aws/auth/variables.tf
+++ iac/terraform/aws/auth/variables.tf
@@ -2,19 +2,3 @@ variable "environment" {
   type        = string
   description = "Deployment environment"
 }
-
-variable "lambda_runtime" {
-  type        = string
-  description = "Lambda Runtime"
-  default     = "nodejs20.x"
-}
-
-variable "api_gw_rest_api_id" {
-  type        = string
-  description = "API Gateway REST API id"
-}
-
-variable "api_gw_root_resource_id" {
-  type        = string
-  description = "API Gateway REST API Root resource id"
-}
