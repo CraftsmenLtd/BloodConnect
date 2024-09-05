9e5e595531a0bc0e9dd642738c25ba6b51344186
mahinuzzaman <mahinuzzaman.md@gmail.com>
2024-09-05 16:46:25 +0600

    I-#105: Update openapi redocly bundle command

--- iac/terraform/aws/openapi.tf
+++ iac/terraform/aws/openapi.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "run_update_and_import_open_api_script" {
   provisioner "local-exec" {
-    command = "redocly bundle ${var.openapi_directory}/versions/${var.openapi_version}.json -o ${var.combined_openapi_file} && cat ${var.combined_openapi_file}"
+    command = "redocly bundle ${var.openapi_directory}/versions/${var.openapi_version}.json -o ${var.combined_openapi_file}"
   }
 
   triggers = {
