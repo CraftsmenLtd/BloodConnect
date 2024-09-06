d45ce7c4e7ac5c1f7e69ec2e9a040f0d65d1f069
Melon <melon.ruet@gmail.com>
2024-09-06 21:07:02 +0600

    I-#105: use data

--- iac/terraform/aws/api_gw.tf
+++ iac/terraform/aws/api_gw.tf
@@ -5,19 +5,7 @@ resource "aws_api_gateway_rest_api" "rest_api" {
     "application/binary",
     "application/bxf+xml",
   ]
-  body = templatefile(
-    var.combined_openapi_file,
-    merge({
-        ENVIRONMENT = var.environment
-        API_VERSION = var.api_version
-      },
-      local.all_lambda_invoke_arns
-    )
-  )
-
-  depends_on = [
-    null_resource.update_and_import_open_api_script
-  ]
+  body = data.template_file.openapi_definition.rendered
 
   lifecycle {
     create_before_destroy = true
--- iac/terraform/aws/openapi.tf
+++ iac/terraform/aws/openapi.tf
@@ -7,3 +7,18 @@ resource "null_resource" "update_and_import_open_api_script" {
     always_run = timestamp()
   }
 }
+
+data "template_file" "openapi_definition" {
+  template = file(var.combined_openapi_file)
+
+  vars = merge({
+    ENVIRONMENT = var.environment
+    API_VERSION = var.api_version
+    },
+    local.all_lambda_invoke_arns
+  )
+
+  depends_on = [
+    null_resource.update_and_import_open_api_script
+  ]
+}
