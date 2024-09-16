80d6d12e17926333780cde3986c8094b76afefb0
biplob14 <biploburrahaman@gmail.com>
2024-09-16 12:46:46 +0600

    I-#74 patch: acm certificate different region include

--- iac/terraform/aws/acm.tf
+++ iac/terraform/aws/acm.tf
@@ -2,6 +2,10 @@ data "aws_acm_certificate" "certificate" {
   domain      = var.bloodconnect_domain
   statuses    = ["ISSUED"]
   most_recent = true
+  provider = aws.virginia
+}
 
-  provider = aws.us-east-1
+provider "aws" {
+  alias = "virginia"
+  region = "us-east-1"
 }
