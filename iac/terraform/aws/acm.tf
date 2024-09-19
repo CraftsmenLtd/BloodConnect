ab8bbb183e76df3d9412611f9405320d8d0b27e0
biplob14 <biploburrahaman@gmail.com>
2024-09-19 13:02:14 +0600

    I-#74 style: formatting terraform

--- iac/terraform/aws/acm.tf
+++ iac/terraform/aws/acm.tf
@@ -2,10 +2,10 @@ data "aws_acm_certificate" "certificate" {
   domain      = var.bloodconnect_domain
   statuses    = ["ISSUED"]
   most_recent = true
-  provider = aws.virginia
+  provider    = aws.virginia
 }
 
 provider "aws" {
-  alias = "virginia"
+  alias  = "virginia"
   region = "us-east-1"
 }
