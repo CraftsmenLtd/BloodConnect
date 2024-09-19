7c660ccf05e39e6474fa4e6e0b79cacac6e28ee3
mahinuzzaman <mahinuzzaman.md@gmail.com>
2024-09-19 13:33:54 +0600

    I-#75: Added api config in shared directory

---
+++ clients/shared/config/apiConfig.ts
@@ -0,0 +1,24 @@
+import { fetchAuthSession } from '@aws-amplify/auth'
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
+
+api.interceptors.request.use(
+  async(config) => {
+    const idToken = (
+      await fetchAuthSession()
+    ).tokens?.idToken?.toString()
+    if (idToken !== undefined && idToken !== null) {
+      config.headers.Authorization = `Bearer ${idToken}`
+    }
+    return config
+  },
+  async(error) => await Promise.reject(error)
+)
+
+export default api
---
+++ clients/shared/services/api/apiMethods.ts
@@ -0,0 +1,35 @@
+import api from '../../config/apiConfig'
+import { AxiosResponse } from 'axios'
+
+export const get = async(url: string, params?: object): Promise<AxiosResponse> => {
+  const response = await api.get(url, { params })
+  return response
+}
+
+export const getSingle = async(
+  url: string,
+  id: string | number,
+  params?: object
+): Promise<AxiosResponse> => {
+  const response = await api.get(`${url}/${id}`, { params })
+  return response
+}
+
+export const post = async(url: string, data: object): Promise<AxiosResponse> => {
+  const response = await api.post(url, data)
+  return response
+}
+
+export const put = async(
+  url: string,
+  id: string | number,
+  data: object
+): Promise<AxiosResponse> => {
+  const response = await api.put(`${url}/${id}`, data)
+  return response
+}
+
+export const del = async(url: string, id: string | number): Promise<AxiosResponse> => {
+  const response = await api.delete(`${url}/${id}`)
+  return response
+}
