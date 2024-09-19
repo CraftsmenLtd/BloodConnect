66d8f149149c0eeafc092e7f2016ef21c437244a
mahinuzzaman <mahinuzzaman.md@gmail.com>
2024-09-19 15:21:25 +0600

    I-#75: Added components for alert, button, inputfield, loader

---
+++ clients/web/src/presentation/components/input-fields/PasswordInput.tsx
@@ -0,0 +1,82 @@
+import React, { ChangeEvent, useState } from 'react';
+import { FaRegEye, FaRegEyeSlash } from '@/presentation/assets/icons';
+import InputField from '@/presentation/components/input-fields/index';
+import {
+  PasswordState,
+  PasswordValidator,
+} from '@/presentation/components/validators/Password';
+
+interface PasswordFieldProps {
+  label: string;
+  value: string;
+  onChange: (value: string) => void;
+  className?: string;
+  placeholder: string;
+}
+
+const PasswordField: React.FC<PasswordFieldProps> = ({
+  label,
+  value,
+  onChange,
+  placeholder,
+}) => {
+  const [validation, setValidation] = useState<PasswordState>({
+    length: false,
+    uppercase: false,
+    lowercase: false,
+    number: false,
+    symbol: false,
+  });
+  const [showValidation, setShowValidation] = useState<boolean>(false);
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.target.value;
+    onChange(newValue);
+    const newValidation = {
+      length: newValue.length >= 8,
+      uppercase: /[A-Z]/.test(newValue),
+      lowercase: /[a-z]/.test(newValue),
+      number: /\d/.test(newValue),
+      symbol: /[^\w\s]/.test(newValue),
+    };
+    setValidation(newValidation);
+    setShowValidation(!Object.values(newValidation).every(Boolean));
+  };
+
+  const allPasswordConditionsMet = Object.values(validation).every(Boolean);
+
+  return (
+    <div className="relative">
+      <InputField
+        type={showPassword ? 'text' : 'password'}
+        placeholder={placeholder}
+        label={label}
+        value={value}
+        onChange={handleChange}
+        icon={
+          showPassword ? (
+            <FaRegEyeSlash
+              onClick={() => {
+                setShowPassword(!showPassword);
+              }}
+              size={22}
+            />
+          ) : (
+            <FaRegEye
+              onClick={() => {
+                setShowPassword(!showPassword);
+              }}
+              size={22}
+            />
+          )
+        }
+      />
+      {!allPasswordConditionsMet && showValidation && (
+        <PasswordValidator validation={validation} />
+      )}
+    </div>
+  );
+};
+
+export default PasswordField;
