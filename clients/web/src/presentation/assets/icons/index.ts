4d5d7c57188f8b01ebdfbb688608e6a86a5eebc6
mahinuzzaman <mahinuzzaman.md@gmail.com>
2024-09-19 13:36:49 +0600

    I-#75: Added assets for logo and icons

---
+++ clients/web/src/presentation/assets/icons/index.ts
@@ -0,0 +1,42 @@
+import { CgOrganisation } from 'react-icons/cg';
+import { CiUser } from 'react-icons/ci';
+import {
+  FaAlignLeft,
+  FaAngleDown,
+  FaRegEye,
+  FaRegEyeSlash,
+  FaUpload,
+} from 'react-icons/fa6';
+import { ImCross } from 'react-icons/im';
+import {
+  MdOutlineMail,
+  MdArrowDropDown,
+  MdArrowBack,
+  MdMenu,
+  MdSettings,
+  MdLogout,
+  MdDashboard,
+} from 'react-icons/md';
+import { HiOutlinePhone } from 'react-icons/hi';
+import { FaSun, FaMoon } from 'react-icons/fa';
+
+export {
+  CgOrganisation,
+  CiUser,
+  FaAlignLeft,
+  FaAngleDown,
+  FaRegEye,
+  FaRegEyeSlash,
+  FaUpload,
+  ImCross,
+  MdOutlineMail,
+  HiOutlinePhone,
+  MdArrowDropDown,
+  MdArrowBack,
+  MdMenu,
+  MdSettings,
+  MdDashboard,
+  MdLogout,
+  FaSun,
+  FaMoon,
+};
