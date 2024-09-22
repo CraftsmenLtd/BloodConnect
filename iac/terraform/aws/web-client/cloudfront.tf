2d40eb87ad14e43378595fda4953939f87e65119
biplob14 <biploburrahaman@gmail.com>
2024-09-22 23:55:34 +0600

    I-#131 feat: geo restriction to cloudfront access

--- iac/terraform/aws/web-client/cloudfront.tf
+++ iac/terraform/aws/web-client/cloudfront.tf
@@ -69,7 +69,8 @@ resource "aws_cloudfront_distribution" "cdn" {
 
   restrictions {
     geo_restriction {
-      restriction_type = "none"
+      restriction_type = "whitelist"
+      locations = [ "BD" ]
     }
   }
 }
