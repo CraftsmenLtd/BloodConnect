44c12ac42250376501e4b17af5e114878455ad4e
biplob14 <biploburrahaman@gmail.com>
2024-09-24 18:09:50 +0600

    I-#74 fix: include abort incomplete multipart upload

--- iac/terraform/aws/web-client/s3.tf
+++ iac/terraform/aws/web-client/s3.tf
@@ -39,6 +39,9 @@ resource "aws_s3_bucket_lifecycle_configuration" "log_store_lifecycle" {
     filter {
       prefix = ""
     }
+    abort_incomplete_multipart_upload {
+      days_after_initiation = 1
+    }
   }
 }
 
