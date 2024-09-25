5e5e27ad58e89cd7740133fc86f9c1547cb1090c
mahinuzzaman <mahinuzzaman.md@gmail.com>
2024-09-26 00:07:09 +0600

    I-#75: Added dotenv to get environment variables

--- clients/web/src/config/awsExports.ts
+++ clients/web/src/config/awsExports.ts
@@ -1,9 +1,8 @@
 const awsExports = {
-  aws_project_region: import.meta.env.VITE_AWS_REGION,
-  aws_cognito_region: import.meta.env.VITE_AWS_COGNITO_REGION,
-  aws_user_pools_id: import.meta.env.VITE_AWS_USER_POOL_ID,
-  aws_user_pools_web_client_id: import.meta.env
-    .VITE_AWS_USER_POOL_WEB_CLIENT_ID
-}
+  aws_project_region: process.env.AWS_REGION,
+  aws_cognito_region: process.env.AWS_COGNITO_REGION,
+  aws_user_pools_id: process.env.AWS_USER_POOL_ID,
+  aws_user_pools_web_client_id: process.env.AWS_USER_POOL_WEB_CLIENT_ID,
+};
 
-export default awsExports
+export default awsExports;
--- clients/web/vite.config.ts
+++ clients/web/vite.config.ts
@@ -3,6 +3,9 @@ import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
   plugins: [
@@ -22,5 +25,15 @@ export default defineConfig({
       '@shared': resolve(__dirname, '../shared'),
       '@commons': resolve(__dirname, '../../commons')
     }
+  },
+  define: {
+    'process.env': {
+      AWS_REGION: process.env.AWS_REGION,
+      AWS_COGNITO_REGION: process.env.AWS_COGNITO_REGION,
+      AWS_USER_POOL_ID: process.env.AWS_USER_POOL_ID,
+      AWS_USER_POOL_WEB_CLIENT_ID: process.env.AWS_USER_POOL_WEB_CLIENT_ID,
+      PASSWORD_MINIMUM_LENGTH: process.env.PASSWORD_MINIMUM_LENGTH,
+      API_URL: process.env.API_URL,
+    },
   }
 })
