cbdd6fefcbb1d32acda2d236b17578a494847b42
mahinuzzaman <mahinuzzaman.md@gmail.com>
2024-09-25 15:31:45 +0600

    I-#75: Removed redux store

--- clients/web/src/main.tsx
+++ clients/web/src/main.tsx
@@ -4,8 +4,6 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Amplify } from 'aws-amplify';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import store from '@web/redux/store';
 import App from './App';
 
 const queryClient = new QueryClient();
@@ -20,9 +18,7 @@ if (rootElement === null) {
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <Provider store={store}>
         <App />
-      </Provider>
     </QueryClientProvider>
   </React.StrictMode>
 );
