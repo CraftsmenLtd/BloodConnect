d344a11c78d029ba310b8cc5e21576ca592d6e91
mahinuzzaman <mahinuzzaman.md@gmail.com>
2024-09-30 16:43:36 +0600

    I-#118: Added deploy Prod

--- iac/terraform/aws/web-client/local.tf
+++ iac/terraform/aws/web-client/local.tf
@@ -1,3 +1,3 @@
 locals {
-  web_client_domain = var.environment == module.environments.PROD ? var.bloodconnect_domain : "${var.environment}.${var.bloodconnect_domain}"
+  web_client_domain = var.environment == module.environments.PRODUCTION ? var.bloodconnect_domain : "${var.environment}.${var.bloodconnect_domain}"
 }
