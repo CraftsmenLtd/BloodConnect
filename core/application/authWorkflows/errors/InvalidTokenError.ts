2f2a17a407699f51b09586fcef6a4241f9f3ae5d
biplob14 <biploburrahaman@gmail.com>
2024-10-08 00:24:00 +0600

    I-#80 feat: removed custom auth lambda code

--- core/application/authWorkflows/errors/InvalidTokenError.ts
+++
@@ -1,8 +0,0 @@
-import ApplicationError from '@commons/libs/errors/ApplicationError'
-import { GENERIC_CODES } from '@commons/libs/constants/GenericCodes'
-
-export default class InvalidTokenError extends ApplicationError {
-  constructor(message: string) {
-    super('InvalidTokenError', message, GENERIC_CODES.UNAUTHORIZED)
-  }
-}
