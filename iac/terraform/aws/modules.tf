a0e4d6f1e13d3a1f00c5cc43e9b51a273e4ce3b1
Ebrahim <ebrahimali.cse.ru@gmail.com>
2024-10-14 16:56:15 +0600

    I-#174: Create two lambda funciton for create and update donation request.

--- iac/terraform/aws/modules.tf
+++ iac/terraform/aws/modules.tf
@@ -8,7 +8,18 @@ module "auth" {
   lambda_archive_path = local.lambda_archive_path
 }
 
+<<<<<<< Updated upstream
 module "web-client" {
+=======
+module "blood_donation" {
+  source              = "./donation"
+  environment         = var.environment
+  lambda_archive_path = local.lambda_archive_path
+  dynamodb_table_arn  = module.database.dynamodb_table_arn
+}
+
+module "web_client" {
+>>>>>>> Stashed changes
   source      = "./web-client"
   environment = var.environment
 }
@@ -18,9 +29,15 @@ module "cloudfront" {
   environment                     = var.environment
   acm_certificate_arn             = data.aws_acm_certificate.certificate.arn
   rest_api_id                     = aws_api_gateway_rest_api.rest_api.id
+<<<<<<< Updated upstream
   static_site_bucket              = module.web-client.static_site_bucket
   failover_bucket                 = module.web-client.failover_bucket
   log_store_bucket                = module.web-client.log_store_bucket
+=======
+  static_site_bucket              = module.web_client.static_site_bucket
+  failover_bucket                 = module.web_client.failover_bucket
+  log_store_bucket                = module.web_client.log_store_bucket
+>>>>>>> Stashed changes
   bloodconnect_environment_domain = local.bloodconnect_environment_domain
 }
 
