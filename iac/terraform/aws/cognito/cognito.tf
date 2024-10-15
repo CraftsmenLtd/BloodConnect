7953f90012a04fbddf1a0f420d26b5093c3f45d2
biplob14 <biploburrahaman@gmail.com>
2024-10-15 14:59:48 +0600

    I-#175 refactor: name change for variable detecting production environment

--- iac/terraform/aws/cognito/cognito.tf
+++ iac/terraform/aws/cognito/cognito.tf
@@ -94,7 +94,7 @@ resource "aws_cognito_identity_provider" "facebook" {
 }
 
 resource "aws_cognito_user_pool_domain" "set_custom_domain_prod" {
-  count = local.isProduction
+  count = local.productionEnvironment
 
   domain          = var.bloodconnect_domain
   user_pool_id    = aws_cognito_user_pool.user_pool.id
@@ -102,18 +102,17 @@ resource "aws_cognito_user_pool_domain" "set_custom_domain_prod" {
 }
 
 resource "aws_cognito_user_pool_domain" "set_custom_domain" {
-  count = local.isNonProduction
+  count = local.NonProductionEnvironment
 
   domain          = local.cognito_domain_name
   user_pool_id    = aws_cognito_user_pool.user_pool.id
-  certificate_arn = var.acm_certificate_arn
 }
 
 resource "aws_route53_record" "cognito_user_pool_custom_domain" {
-  count = local.isProduction
+  count = local.productionEnvironment
 
   name    = aws_cognito_user_pool_domain.set_custom_domain_prod[0].domain
-  type    = "A"
+  type    = "CNAME"
   zone_id = var.hosted_zone_id
   alias {
     evaluate_target_health = false
