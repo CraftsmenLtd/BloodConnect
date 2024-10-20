e129405bc3e5bcff28edc3fb2ab5899d1ad25317
Shahed Chy Suzan <shahedchysuzan@gmail.com>
2024-10-21 03:14:41 +0600

    I-#83: policy added to eventBridge

--- iac/terraform/aws/eventbridge.tf
+++ iac/terraform/aws/eventbridge.tf
@@ -7,6 +7,7 @@ resource "aws_cloudwatch_event_rule" "donation_request_rule" {
     detail-type = ["AWS DynamoDB Stream Event"]
     detail = {
       eventName = ["INSERT", "MODIFY"]
+      tableName = [aws_dynamodb_table.blood_connect_data.name]
       dynamodb = {
         Keys = {
           PK = {
@@ -31,6 +32,7 @@ resource "aws_cloudwatch_event_target" "donor_search_queue_target" {
   rule      = aws_cloudwatch_event_rule.donation_request_rule.name
   target_id = "DonorSearchQueue"
   arn       = aws_sqs_queue.donor_search_queue.arn
+  role_arn  = aws_iam_role.eventbridge_role.arn
 }
 
 # SQS Queue Policy
@@ -60,8 +62,7 @@ resource "aws_sqs_queue_policy" "donor_search_queue_policy" {
 
 # CloudWatch Log Group
 resource "aws_cloudwatch_log_group" "eventbridge_log_group" {
-  name              = "/aws/events/${aws_cloudwatch_event_rule.donation_request_rule.name}"
-  retention_in_days = 14
+  name = "/aws/events/${aws_cloudwatch_event_rule.donation_request_rule.name}"
 }
 
 # EventBridge Target (CloudWatch Logs)
@@ -104,6 +105,48 @@ resource "aws_iam_role_policy" "eventbridge_cloudwatch_policy" {
           "logs:PutLogEvents"
         ]
         Resource = "${aws_cloudwatch_log_group.eventbridge_log_group.arn}:*"
+      }
+    ]
+  })
+}
+
+
+resource "aws_iam_role" "eventbridge_role" {
+  name = "${var.environment}-eventbridge-dynamodb-stream-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "events.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "eventbridge_policy" {
+  name = "${var.environment}-eventbridge-dynamodb-stream-policy"
+  role = aws_iam_role.eventbridge_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:GetRecords",
+          "dynamodb:GetShardIterator",
+          "dynamodb:DescribeStream",
+          "dynamodb:ListStreams"
+        ]
+        Resource = aws_dynamodb_table.blood_connect_data.stream_arn
+      },
+      {
+        Effect = "Allow"
+        Action = ["sqs:SendMessage"]
+        Resource = aws_sqs_queue.donor_search_queue.arn
       }
     ]
   })
