a162116f6f5acc345f55f5dbe870ad3ff37230c1
Shahed Chy Suzan <shahedchysuzan@gmail.com>
2024-10-20 23:54:20 +0600

    I-#83: eventBridge rule updated

--- iac/terraform/aws/eventbridge.tf
+++ iac/terraform/aws/eventbridge.tf
@@ -1,19 +1,15 @@
-resource "aws_cloudwatch_event_rule" "blood_request_stream_rule" {
-  name        = "${var.environment}-blood-request-stream-rule"
-  description = "Rule to capture blood request post events from DynamoDB stream"
+resource "aws_cloudwatch_event_rule" "donation_request_rule" {
+  name        = "${var.environment}-donation-request-rule"
+  description = "Capture blood donation request creations and updates"
 
   event_pattern = jsonencode({
     source      = ["aws.dynamodb"]
-    detail-type = ["AWS API Call via CloudTrail"]
+    detail-type = ["AWS DynamoDB Stream Event"]
     detail = {
-      eventSource = ["dynamodb.amazonaws.com"]
-      eventName   = ["INSERT", "MODIFY"]
+      eventName = ["INSERT", "MODIFY"]
       dynamodb = {
         Keys = {
           PK = {
-            S = [{prefix = "BLOOD_REQ#"}]
-          }
-          SK = {
             S = [{prefix = "BLOOD_REQ#"}]
           }
         }
@@ -22,8 +18,40 @@ resource "aws_cloudwatch_event_rule" "blood_request_stream_rule" {
   })
 }
 
-resource "aws_cloudwatch_event_target" "blood_request_sqs_target" {
-  rule      = aws_cloudwatch_event_rule.blood_request_stream_rule.name
-  target_id = "SendToBloodRequestQueue"
+resource "aws_sqs_queue" "donor_search_queue" {
+  name                      = "${var.environment}-donor-search-queue"
+  delay_seconds             = 0
+  max_message_size          = 262144
+  message_retention_seconds = 86400
+  receive_wait_time_seconds = 10
+}
+
+resource "aws_cloudwatch_event_target" "donor_search_queue_target" {
+  rule      = aws_cloudwatch_event_rule.donation_request_rule.name
+  target_id = "DonorSearchQueue"
   arn       = aws_sqs_queue.donor_search_queue.arn
+}
+
+resource "aws_sqs_queue_policy" "donor_search_queue_policy" {
+  queue_url = aws_sqs_queue.donor_search_queue.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowEventBridgeSendMessage"
+        Effect = "Allow"
+        Principal = {
+          Service = "events.amazonaws.com"
+        }
+        Action   = "sqs:SendMessage"
+        Resource = aws_sqs_queue.donor_search_queue.arn
+        Condition = {
+          ArnEquals = {
+            "aws:SourceArn" = aws_cloudwatch_event_rule.donation_request_rule.arn
+          }
+        }
+      }
+    ]
+  })
 }
--- iac/terraform/aws/sqs.tf
+++ iac/terraform/aws/sqs.tf
@@ -1,35 +1,35 @@
-resource "aws_sqs_queue" "donor_search_queue" {
-  name                      = "${var.environment}-donor-search-queue"
-  delay_seconds             = 0
-  max_message_size          = 262144
-  message_retention_seconds = 86400
-  receive_wait_time_seconds = 10
+# resource "aws_sqs_queue" "donor_search_queue" {
+#   name                      = "${var.environment}-donor-search-queue"
+#   delay_seconds             = 0
+#   max_message_size          = 262144
+#   message_retention_seconds = 86400
+#   receive_wait_time_seconds = 10
 
-  tags = {
-    Environment = var.environment
-  }
-}
+#   tags = {
+#     Environment = var.environment
+#   }
+# }
 
-resource "aws_sqs_queue_policy" "donor_search_queue_policy" {
-  queue_url = aws_sqs_queue.donor_search_queue.id
+# resource "aws_sqs_queue_policy" "donor_search_queue_policy" {
+#   queue_url = aws_sqs_queue.donor_search_queue.id
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid    = "AllowEventBridgeSendMessage"
-        Effect = "Allow"
-        Principal = {
-          Service = "events.amazonaws.com"
-        }
-        Action   = "sqs:SendMessage"
-        Resource = aws_sqs_queue.donor_search_queue.arn
-        Condition = {
-          ArnEquals = {
-            "aws:SourceArn" = aws_cloudwatch_event_rule.blood_request_stream_rule.arn
-          }
-        }
-      }
-    ]
-  })
-}
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Sid    = "AllowEventBridgeSendMessage"
+#         Effect = "Allow"
+#         Principal = {
+#           Service = "events.amazonaws.com"
+#         }
+#         Action   = "sqs:SendMessage"
+#         Resource = aws_sqs_queue.donor_search_queue.arn
+#         Condition = {
+#           ArnEquals = {
+#             "aws:SourceArn" = aws_cloudwatch_event_rule.blood_request_stream_rule.arn
+#           }
+#         }
+#       }
+#     ]
+#   })
+# }
