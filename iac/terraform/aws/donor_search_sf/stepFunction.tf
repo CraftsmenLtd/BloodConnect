0284bd26fd62734b9fb727a80b80472aa999819c
mahinuzzaman <mahinuzzaman.md@gmail.com>
2024-10-22 05:57:40 +0600

    I-#154: Fixed StepFunction Role issue and skipped localstack deployment for stage

--- iac/terraform/aws/donor_search_sf/stepFunction.tf
+++ iac/terraform/aws/donor_search_sf/stepFunction.tf
@@ -35,16 +35,16 @@ resource "aws_iam_policy" "step_function_logging_policy" {
     {
       "Effect": "Allow",
       "Action": [
-          "logs:CreateLogDelivery",
-          "logs:GetLogDelivery",
-          "logs:UpdateLogDelivery",
-          "logs:DeleteLogDelivery",
-          "logs:ListLogDeliveries",
-          "logs:PutResourcePolicy",
-          "logs:DescribeResourcePolicies",
-          "logs:DescribeLogGroups"
+        "logs:CreateLogDelivery",
+        "logs:GetLogDelivery",
+        "logs:UpdateLogDelivery",
+        "logs:DeleteLogDelivery",
+        "logs:ListLogDeliveries",
+        "logs:PutResourcePolicy",
+        "logs:DescribeResourcePolicies",
+        "logs:DescribeLogGroups"
       ],
-      "Resource": "${aws_cloudwatch_log_group.donor_search_state_machine_logs.arn}:*"
+      "Resource": "*"
     }
   ]
 }
