789616b7d4bd5dcd8765f0af436e272fc3bd8ffe
Biplob14 <biploburrahaman@gmail.com>
2024-10-29 15:04:41 +0600

    I-85 feat: unit test for accept donation request service

---
+++ core/application/tests/bloodDonationWorkflow/AcceptDonationRequestService.test.ts
@@ -0,0 +1,81 @@
+import { AcceptDonationService } from '../../../application/bloodDonationWorkflow/AcceptDonationRequestService'
+import { GENERIC_CODES } from '../../../../commons/libs/constants/GenericCodes'
+import AcceptDonationRequestError from '../../../application/bloodDonationWorkflow/AcceptDonationRequestError'
+import { AcceptedDonationDTO, DonationStatus } from '../../../../commons/dto/DonationDTO'
+import { acceptDonationRequestAttributesMock } from '../../tests/mocks/mockDonationAcceptanceData'
+
+jest.mock('../../technicalImpl/policies/repositories/Repository')
+
+const mockAcceptDonationRepository = {
+  create: jest.fn(),
+  update: jest.fn(),
+  getItem: jest.fn()
+}
+const acceptDonationService = new AcceptDonationService()
+
+describe('AcceptDonationService', () => {
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('should accept a donation request successfully when status is pending', async() => {
+    const mockQueryResult: AcceptedDonationDTO = {
+      ...acceptDonationRequestAttributesMock,
+      status: DonationStatus.PENDING
+    }
+
+    mockAcceptDonationRepository.getItem.mockResolvedValue(mockQueryResult)
+
+    const result = await acceptDonationService.createAcceptanceRecord(
+      acceptDonationRequestAttributesMock,
+      mockAcceptDonationRepository
+    )
+
+    expect(result).toBe('Donation request accepted successfully.')
+    expect(mockAcceptDonationRepository.create).toHaveBeenCalledWith({
+      donorId: acceptDonationRequestAttributesMock.donorId,
+      seekerId: acceptDonationRequestAttributesMock.seekerId,
+      createdAt: acceptDonationRequestAttributesMock.createdAt,
+      requestPostId: acceptDonationRequestAttributesMock.requestPostId,
+      acceptanceTime: expect.any(String) // acceptanceTime should be a valid date string
+    })
+  })
+
+  it('should return an error message when donation request is no longer available for acceptance', async() => {
+    const mockQueryResult: AcceptedDonationDTO = {
+      ...acceptDonationRequestAttributesMock,
+      status: DonationStatus.COMPLETED
+    }
+
+    mockAcceptDonationRepository.getItem.mockResolvedValue(mockQueryResult)
+
+    const result = await acceptDonationService.createAcceptanceRecord(
+      acceptDonationRequestAttributesMock,
+      mockAcceptDonationRepository
+    )
+
+    expect(result).toBe('Donation request is no longer available for acceptance.')
+    expect(mockAcceptDonationRepository.create).not.toHaveBeenCalled()
+  })
+
+  it('should return an error message when the donation request cannot be found', async() => {
+    mockAcceptDonationRepository.getItem.mockResolvedValue(null)
+
+    const result = await acceptDonationService.createAcceptanceRecord(
+      acceptDonationRequestAttributesMock,
+      mockAcceptDonationRepository
+    )
+
+    expect(result).toBe('Cannot find the donation request')
+    expect(mockAcceptDonationRepository.create).not.toHaveBeenCalled()
+  })
+
+  it('should throw an AcceptDonationRequestError when an error occurs', async() => {
+    const mockError = new Error('Database error')
+    mockAcceptDonationRepository.getItem.mockRejectedValue(mockError)
+
+    await expect(
+      acceptDonationService.createAcceptanceRecord(acceptDonationRequestAttributesMock, mockAcceptDonationRepository)
+    ).rejects.toThrow(new AcceptDonationRequestError(`Failed to accept donation request. Error: ${mockError}`, GENERIC_CODES.ERROR))
+  })
+})
