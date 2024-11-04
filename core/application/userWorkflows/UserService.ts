56903fd15561f60560cb2cc579c564555fe9cd7e
Ebrahim <ebrahimali.cse.ru@gmail.com>
2024-11-04 17:37:02 +0600

    I-#160: Create lambda and write service for notification

--- core/application/userWorkflows/UserService.ts
+++ core/application/userWorkflows/UserService.ts
@@ -6,10 +6,15 @@ import { GenericMessage } from '../../../commons/dto/MessageDTO'
 import { getEmailVerificationMessage, getPasswordResetVerificationMessage, getAppUserWellcomeMailMessage } from './userMessages'
 import Repository from '../technicalImpl/policies/repositories/Repository'
 
-type UserAttributes = {
+interface UserAttributes {
   email: string;
   name: string;
   phone_number: string;
+}
+
+interface NotificationAttributes {
+  userId: string;
+  endpointArn: string;
 }
 
 export class UserService {
@@ -25,6 +30,22 @@ export class UserService {
     } catch (error) {
       throw new UserOperationError(`Failed to create new user. Error: ${error}`, GENERIC_CODES.ERROR)
     }
+  }
+
+  async storeEndpointArn(notificationAttributes: NotificationAttributes, userRepository: Repository<UserDTO>): Promise<void> {
+    const { userId, endpointArn } = notificationAttributes
+
+    const item = await userRepository.getItem(`USER#${userId}`, 'PROFILE')
+
+    if (item === null) {
+      throw new Error('Item not found.')
+    }
+
+    const updateData: Partial<UserDTO> = {
+      id: userId,
+      endpointArn
+    }
+    await userRepository.update(updateData)
   }
 
   getPostSignUpMessage(userName: string, securityCode: string): GenericMessage {
