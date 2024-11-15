fbeece8add16c375ebd7b7d4491dd06ac03015de
Shahed Chy Suzan <shahedchysuzan@gmail.com>
2024-11-15 14:11:30 +0600

    I-#206: dropdown label removed

--- clients/mobile/app.config.ts
+++ clients/mobile/app.config.ts
@@ -16,6 +16,8 @@ export default ({ config }: { config: ExpoConfig }): ExpoConfig => {
     GOOGLE_MAP_API_KEY?: string;
     GOOGLE_MAP_API?: string;
     OPENSTREET_MAP_API?: string;
+    APP_ENV?: string;
+    COUNTRY?: string;
   }> = {
     development: {
       AWS_USER_POOL_ID: process.env.AWS_USER_POOL_ID,
@@ -28,7 +30,24 @@ export default ({ config }: { config: ExpoConfig }): ExpoConfig => {
       LOCATION_SERVICE_EMAIL: process.env.LOCATION_SERVICE_EMAIL,
       GOOGLE_MAP_API_KEY: process.env.GOOGLE_MAP_API_KEY,
       GOOGLE_MAP_API: process.env.GOOGLE_MAP_API,
-      OPENSTREET_MAP_API: process.env.OPENSTREET_MAP_API
+      OPENSTREET_MAP_API: process.env.OPENSTREET_MAP_API,
+      APP_ENV: process.env.APP_ENV,
+      COUNTRY: process.env.COUNTRY
+    },
+    preview: {
+      AWS_USER_POOL_ID: process.env.AWS_USER_POOL_ID,
+      AWS_USER_POOL_CLIENT_ID: process.env.AWS_USER_POOL_CLIENT_ID,
+      AWS_COGNITO_DOMAIN: process.env.AWS_COGNITO_DOMAIN,
+      EAS_PROJECT_ID: process.env.EAS_PROJECT_ID,
+      API_BASE_URL: process.env.API_BASE_URL,
+      APP_NAME: process.env.APP_NAME,
+      APP_VERSION: process.env.APP_VERSION,
+      LOCATION_SERVICE_EMAIL: process.env.LOCATION_SERVICE_EMAIL,
+      GOOGLE_MAP_API_KEY: process.env.GOOGLE_MAP_API_KEY,
+      GOOGLE_MAP_API: process.env.GOOGLE_MAP_API,
+      OPENSTREET_MAP_API: process.env.OPENSTREET_MAP_API,
+      APP_ENV: process.env.APP_ENV,
+      COUNTRY: process.env.COUNTRY
     }
   }
 
