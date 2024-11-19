a6d01e13a986ddd584d6bfd88516a0a1e9898af6
Biplob14 <biploburrahaman@gmail.com>
2024-11-19 11:52:25 +0600

    I-#167 fix: linting issue

--- clients/mobile/src/setup/notification/useNavigationReady.ts
+++ clients/mobile/src/setup/notification/useNavigationReady.ts
@@ -4,7 +4,7 @@ export const useNavigationReady = (navigation: NavigationProp<any>) => {
   return async(): Promise<void> => {
     let attempts = 0
     while (attempts < 10) {
-      if (navigation.isReady()) {
+      if (navigation.isReady() !== null) {
         return
       }
       await new Promise(resolve => setTimeout(resolve, 500))
