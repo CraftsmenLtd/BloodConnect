724c063581a85fa084b8c7a30ce616fa51c9c83a
sakibstark11 <16sakib@gmail.com>
2024-11-29 22:12:31 +0000

    NO-TICKET: fixes broken localstack

--- core/application/authWorkflow/authToken/constants.ts
+++
@@ -1,4 +0,0 @@
-import Environments from '../../../../commons/libs/constants/Environments'
-
-export const jwtSecret = process.env.JWT_SECRET ?? 'secret'
-export const executionEnv = (process.env.ENVIRONMENT ?? Environments.LOCAL) as Environments
