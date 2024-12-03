19dd16d8705a837fb1bd73be1c7925facea60f42
Shahed Chy Suzan <shahedchysuzan@gmail.com>
2024-12-03 14:58:59 +0600

    I-#231: Added a type declaration for image imports

--- clients/mobile/src/authentication/socialAuth/constants/socialButtonUI.ts
+++ clients/mobile/src/authentication/socialAuth/constants/socialButtonUI.ts
@@ -1,10 +1,13 @@
+import googleIcon from '../../../../assets/google-icon.png'
+import facebookIcon from '../../../../assets/facebook-icon.png'
+
 export const SOCIAL_BUTTON_UI = {
   GOOGLE: {
-    icon: require('../../../../assets/google-icon.png'),
+    icon: googleIcon,
     text: 'Continue with Google'
   },
   FACEBOOK: {
-    icon: require('../../../../assets/facebook-icon.png'),
+    icon: facebookIcon,
     text: 'Continue with Facebook'
   }
 } as const
