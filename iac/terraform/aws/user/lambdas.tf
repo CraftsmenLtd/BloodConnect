a1e6accf1f6e30f55802848b73488dfd5315e707
mahinuzzaman <mahinuzzaman.md@gmail.com>
2024-12-04 16:00:14 +0600

    NO-TICKET: fix weird name for donation cool down in tf

--- iac/terraform/aws/user/lambdas.tf
+++ iac/terraform/aws/user/lambdas.tf
@@ -7,8 +7,8 @@ locals {
       statement                  = concat(local.policies.common_policies, local.policies.dynamodb_policy)
       invocation_arn_placeholder = "UPDATE_USER_INVOCATION_ARN"
       env_variables = {
-        DYNAMODB_TABLE_NAME               = split("/", var.dynamodb_table_arn)[1]
-        AFTER_DONATION_UNAVAILABLE_PERIOD = var.after_donation_unavailable_period
+        DYNAMODB_TABLE_NAME          = split("/", var.dynamodb_table_arn)[1]
+        MIN_MONTHS_BETWEEN_DONATIONS = var.min_months_between_donations
       }
     }
   }
