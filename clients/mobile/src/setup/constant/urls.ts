21aa56959efafcd687902a65c3578b4b46b58a25
Shahed Chy Suzan <shahedchysuzan@gmail.com>
2024-12-06 12:34:27 +0600

    I-#261: Added generated link of Terms of Service and Privacy Policy

--- clients/mobile/src/setup/constant/urls.ts
+++ clients/mobile/src/setup/constant/urls.ts
@@ -1,6 +1,6 @@
 export const POLICY_URLS = {
-  TERMS_OF_SERVICE: 'https://craftsmenltd.github.io/BloodConnect/development/MobileAppDevelopment.html',
-  PRIVACY_POLICY: 'https://craftsmenltd.github.io/BloodConnect/development/SSOSetupGuide.html'
+  TERMS_OF_SERVICE: 'https://www.privacypolicies.com/live/2596611c-b265-44cd-9ded-b29af04301a6',
+  PRIVACY_POLICY: 'https://www.privacypolicies.com/live/10d74fb9-4c1f-4447-9efe-3e462f9ef0cd'
 }
 
 export type PolicyUrlType = typeof POLICY_URLS
