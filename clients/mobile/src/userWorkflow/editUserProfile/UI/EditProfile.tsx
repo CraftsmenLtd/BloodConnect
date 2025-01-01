cce390e41b4036a158575c43505a235e5a046ec4
Biplob14 <biploburrahaman@gmail.com>
2025-01-01 16:50:35 +0600

    I-#263 refactor: edit profile indentation

--- clients/mobile/src/userWorkflow/editUserProfile/UI/EditProfile.tsx
+++ clients/mobile/src/userWorkflow/editUserProfile/UI/EditProfile.tsx
@@ -23,118 +23,118 @@ const EditProfile = () => {
 
   return (
     <TouchableWithoutFeedback>
-        <ScrollView
+      <ScrollView
         style={styles.container}
         showsVerticalScrollIndicator={false}
-        >
-            <ProfileSection
-                name={profileData.name}
-                location={profileData.location}
-                isEditing={true}
-            />
+      >
+        <ProfileSection
+          name={profileData.name}
+          location={profileData.location}
+          isEditing={true}
+        />
 
-            <View style={styles.gradientTop} />
-            <View style={styles.gradientBottom} />
-            <View style={styles.scrollContent}>
-                <View style={styles.infoContainer}>
-                    <View style={styles.inputFieldStyle}>
-                        <Input
-                        name="name"
-                        label="Name"
-                        value={profileData.name}
-                        onChangeText={handleInputChange}
-                        placeholder="Enter your name"
-                        inputStyle={styles.inputStyle}
-                        error={errors.name}
-                        />
-                    </View>
-
-                    <View style={styles.inputFieldStyle}>
-                        <DateTimePickerComponent
-                        label="Date of Birth"
-                        value={new Date(profileData.dateOfBirth)}
-                        onChange={handleInputChange}
-                        isOnlyDate={true}
-                        inputStyle={styles.inputStyle}
-                        // showOnlyDate={true}
-                        error={errors.dateOfBirth}
-                        />
-                    </View>
-
-                    <View style={styles.inputFieldStyle}>
-                        <Input
-                        name="age"
-                        label="Age"
-                        value={profileData.age.toString()}
-                        onChangeText={() => {}}
-                        placeholder="Enter your name"
-                        readOnly={true}
-                        inputStyle={styles.inputStyle}
-                        />
-                    </View>
-
-                    <View style={styles.inputFieldStyle}>
-                        <Input
-                        name="weight"
-                        label="Weight (kg)"
-                        value={profileData.weight.toString()}
-                        onChangeText={handleInputChange}
-                        placeholder="Enter your weight"
-                        keyboardType="numeric"
-                        inputStyle={styles.inputStyle}
-                        error={errors.weight}
-                        />
-                    </View>
-
-                    <View style={styles.inputFieldStyle}>
-                        <Input
-                        name="height"
-                        label="Height (feet)"
-                        value={profileData.height.toString()}
-                        onChangeText={handleInputChange}
-                        placeholder="Enter your height"
-                        keyboardType="numeric"
-                        inputStyle={styles.inputStyle}
-                        error={errors.height}
-                        />
-                    </View>
-
-                    <View style={styles.inputFieldStyle}>
-                        <Input
-                        name="phone"
-                        label="Phone"
-                        value={profileData.phone}
-                        onChangeText={handleInputChange}
-                        placeholder="Enter your phone number"
-                        keyboardType="decimal-pad"
-                        inputStyle={styles.inputStyle}
-                        error={errors.phone}
-                        />
-                        <Warning
-                        text={WARNINGS.PHONE_NUMBER_VISIBLE}
-                        showWarning={profileData.phone !== ''}
-                        />
-                    </View>
-
-                    <View style={styles.inputFieldStyle}>
-                        <RadioButton
-                        name="gender"
-                        label="Gender"
-                        options={['female', 'male', 'other']}
-                        value={profileData.gender}
-                        onPress={handleInputChange}
-                        />
-                    </View>
-                </View>
-                <View style={styles.buttonContainer}>
-                <Button
-                    text="Save"
-                    onPress={handleSave}
-                    disabled={isButtonDisabled}
-                />
-                </View>
+        <View style={styles.gradientTop} />
+        <View style={styles.gradientBottom} />
+        <View style={styles.scrollContent}>
+          <View style={styles.infoContainer}>
+            <View style={styles.inputFieldStyle}>
+              <Input
+                name="name"
+                label="Name"
+                value={profileData.name}
+                onChangeText={handleInputChange}
+                placeholder="Enter your name"
+                inputStyle={styles.inputStyle}
+                error={errors.name}
+              />
             </View>
-        </ScrollView>
+
+            <View style={styles.inputFieldStyle}>
+              <DateTimePickerComponent
+                label="Date of Birth"
+                value={new Date(profileData.dateOfBirth)}
+                onChange={handleInputChange}
+                isOnlyDate={true}
+                inputStyle={styles.inputStyle}
+                // showOnlyDate={true}
+                error={errors.dateOfBirth}
+              />
+            </View>
+
+            <View style={styles.inputFieldStyle}>
+              <Input
+                name="age"
+                label="Age"
+                value={profileData.age.toString()}
+                onChangeText={() => { }}
+                placeholder="Enter your name"
+                readOnly={true}
+                inputStyle={styles.inputStyle}
+              />
+            </View>
+
+            <View style={styles.inputFieldStyle}>
+              <Input
+                name="weight"
+                label="Weight (kg)"
+                value={profileData.weight.toString()}
+                onChangeText={handleInputChange}
+                placeholder="Enter your weight"
+                keyboardType="numeric"
+                inputStyle={styles.inputStyle}
+                error={errors.weight}
+              />
+            </View>
+
+            <View style={styles.inputFieldStyle}>
+              <Input
+                name="height"
+                label="Height (feet)"
+                value={profileData.height.toString()}
+                onChangeText={handleInputChange}
+                placeholder="Enter your height"
+                keyboardType="numeric"
+                inputStyle={styles.inputStyle}
+                error={errors.height}
+              />
+            </View>
+
+            <View style={styles.inputFieldStyle}>
+              <Input
+                name="phone"
+                label="Phone"
+                value={profileData.phone}
+                onChangeText={handleInputChange}
+                placeholder="Enter your phone number"
+                keyboardType="decimal-pad"
+                inputStyle={styles.inputStyle}
+                error={errors.phone}
+              />
+              <Warning
+                text={WARNINGS.PHONE_NUMBER_VISIBLE}
+                showWarning={profileData.phone !== ''}
+              />
+            </View>
+
+            <View style={styles.inputFieldStyle}>
+              <RadioButton
+                name="gender"
+                label="Gender"
+                options={['female', 'male', 'other']}
+                value={profileData.gender}
+                onPress={handleInputChange}
+              />
+            </View>
+          </View>
+          <View style={styles.buttonContainer}>
+            <Button
+              text="Save"
+              onPress={handleSave}
+              disabled={isButtonDisabled}
+            />
+          </View>
+        </View>
+      </ScrollView>
     </TouchableWithoutFeedback>
   )
 }
