65edf77ee077998dfa8c513d69b45c0338a23664
sakibstark11 <16sakib@gmail.com>
2025-01-08 17:46:32 +0000

    no-ticket: observ

--- commons/libs/logger/JsonLogger.ts
+++ commons/libs/logger/JsonLogger.ts
@@ -3,4 +3,5 @@ import pino from 'pino/pino'
 export const JsonLogger = pino({
   timestamp: () => `,"time":"${new Date().toISOString()}"`,
   base: null,
+  level: process.env.LOG_LEVEL || 'info'
 })
