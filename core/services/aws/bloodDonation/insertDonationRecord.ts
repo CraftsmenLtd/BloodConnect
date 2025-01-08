d7937dacf857a7e519b7db56fd3917396211ec54
sakibstark11 <16sakib@gmail.com>
2025-01-08 20:32:04 +0000

    fixed file smaller than 5MB

--- core/services/aws/bloodDonation/insertDonationRecord.ts
+++ core/services/aws/bloodDonation/insertDonationRecord.ts
@@ -6,7 +6,11 @@ import {
   UploadPartCommand,
   CompleteMultipartUploadCommand,
   DeleteObjectCommand,
-  NotFound
+  NotFound,
+  UploadPartCopyCommand,
+  UploadPartCopyCommandOutput,
+  UploadPartCommandOutput,
+  GetObjectCommand
 } from '@aws-sdk/client-s3'
 import { createServiceLogger } from '../commons/serviceLogger/ServiceLogger'
 import { Context } from 'aws-lambda'
@@ -28,7 +32,7 @@ const createNewFile = async (fileName: string, geohash: string): Promise<void> =
     }))
 }
 
-const appendGeohasToFile = async (fileName: string, fileContent: string): Promise<void> => {
+const appendGeohasToFileLargerThan5MB = async (fileName: string, fileContent: string, fileSize: number): Promise<void> => {
   const createMultipartUploadResponse = await client.send(
     new CreateMultipartUploadCommand({
       Bucket: bucketName,
@@ -37,16 +41,36 @@ const appendGeohasToFile = async (fileName: string, fileContent: string): Promis
   )
 
   const uploadId = createMultipartUploadResponse.UploadId
-
-  const uploadPartResponse = await client.send(
-    new UploadPartCommand({
+  const uploadExistingFilePromise = client.send(
+    new UploadPartCopyCommand({
       Bucket: bucketName,
       Key: fileName,
       PartNumber: 1,
       UploadId: uploadId,
-      Body: fileContent
+      CopySource: `${bucketName}/${fileName}`,
+      CopySourceRange: `bytes=0-${fileSize - 1}`
+    }))
+
+  const uploadNewPartPromise = client.send(
+    new UploadPartCommand({
+      Bucket: bucketName,
+      Key: fileName,
+      PartNumber: 2,
+      UploadId: uploadId,
+      Body: `\n${fileContent}`
     })
   )
+
+  const parts = await Promise.all([uploadExistingFilePromise, uploadNewPartPromise].map(async (promise, index) => {
+    const response = await promise
+    const isExistingPart = index + 1 === 1
+    return {
+      PartNumber: index + 1,
+      ETag: isExistingPart
+        ? (response as UploadPartCopyCommandOutput).CopyPartResult?.ETag
+        : (response as UploadPartCommandOutput).ETag
+    }
+  }))
 
   await client.send(
     new CompleteMultipartUploadCommand({
@@ -54,51 +78,75 @@ const appendGeohasToFile = async (fileName: string, fileContent: string): Promis
       Key: fileName,
       UploadId: uploadId,
       MultipartUpload: {
-        Parts: [
-          {
-            ETag: uploadPartResponse.ETag,
-            PartNumber: 1
-          }
-        ]
+        Parts: parts
       }
     })
   )
 }
+
+
+const appendGeohasToFileSmallerThan5MB = async (fileName: string, fileContent: string) => {
+  const existingFileResponse = await client.send(
+    new GetObjectCommand({
+      Bucket: bucketName,
+      Key: fileName
+    })
+  )
+  const existingFileContent = await existingFileResponse.Body?.transformToString()
+  const updatedFileContent = `${existingFileContent}\n${fileContent}`
+  await client.send(
+    new PutObjectCommand({
+      Bucket: bucketName,
+      Key: fileName,
+      Body: updatedFileContent
+    })
+  )
+}
+
 
 async function insertDonationRecord(event:
   Array<{ geohash: string; city: string; requestedBloodGroup: string }>,
   context: Context): Promise<void> {
   const serviceLogger = createServiceLogger(context.awsRequestId)
   try {
-    const geohash = event[0].geohash
+    const fileContent = event[0].geohash
     const city = event[0].city
     const requestedBloodGroup = event[0].requestedBloodGroup
-    const signOfRequestedBloodGroup = requestedBloodGroup.slice(-1) as "+" | "-"
+    const signOfRequestedBloodGroup = requestedBloodGroup.slice(-1) as '+' | '-'
     const bloodGroupCharacter = requestedBloodGroup.slice(0, -1)
     const mapOfSigns = {
-      "+": "positive",
-      "-": "negetive"
+      '+': 'positive',
+      '-': 'negetive'
     }
 
     const potentialFileName = `${city}-${bloodGroupCharacter}-${mapOfSigns[signOfRequestedBloodGroup]}.txt`
-    const fileContent = `${geohash}\n`
     try {
       const existingFile = await client.send(new HeadObjectCommand({ Bucket: bucketName, Key: potentialFileName }))
       serviceLogger.debug('found existing file')
-      const fileSize = existingFile.ContentLength
+      const fileSize = existingFile.ContentLength as number
       const currentNumberOfGeohashes = countGeohashesInFile(fileSize)
       if (currentNumberOfGeohashes > maxGeohashToStoreInFile) {
         serviceLogger.debug('deleting expired size')
         await deleteExpiredFile(potentialFileName)
         serviceLogger.debug('creating new file')
-        return await createNewFile(potentialFileName, fileContent)
+        await createNewFile(potentialFileName, fileContent)
+        return
       }
-      serviceLogger.debug('appending to file')
-      return await appendGeohasToFile(potentialFileName, fileContent)
+
+      const isFileGreaterThan5MB = fileSize > 5 * 1024 * 1024
+      if (isFileGreaterThan5MB) {
+        serviceLogger.debug('appending to file larger than 5MB')
+        await appendGeohasToFileLargerThan5MB(potentialFileName, fileContent, fileSize)
+        return
+      }
+      serviceLogger.debug('appending to file smaller than 5mb')
+      await appendGeohasToFileSmallerThan5MB(potentialFileName, fileContent)
+
     } catch (error) {
       if (error instanceof NotFound) {
         serviceLogger.debug('creating new file')
-        return await createNewFile(potentialFileName, fileContent)
+        await createNewFile(potentialFileName, fileContent)
+        return
       }
       throw error
     }
