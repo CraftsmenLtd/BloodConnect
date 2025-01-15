db7d46daf10428a1dd6adeade09436f9799700b7
sakibstark11 <16sakib@gmail.com>
2025-01-15 20:17:19 +0000

    NO-TICKET: remove reserved vars

--- iac/terraform/aws/eventbridge/lambdas.tf
+++ iac/terraform/aws/eventbridge/lambdas.tf
@@ -7,7 +7,6 @@ locals {
       statement = local.policies.common_policies
 
       env_variables = {
-        AWS_REGION          = data.aws_region.current.name
         MAX_GEOHASH_LENGTH  = var.max_geohash_length
         BUCKET_NAME         = aws_s3_bucket.monitor_donation_request.id
         MAX_GEOHASH_STORAGE = var.max_geohash_storage
