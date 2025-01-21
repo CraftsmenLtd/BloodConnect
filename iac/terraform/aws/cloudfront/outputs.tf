9ef845f28ca94dfb3c6ab5ac11a8402b1d662b45
mahinuzzaman <mahinuzzaman.md@gmail.com>
2025-01-21 22:12:23 +0600

    I-#386: Changed the CloudFront config from terraform to CloudFormation

--- iac/terraform/aws/cloudfront/outputs.tf
+++ iac/terraform/aws/cloudfront/outputs.tf
@@ -1,7 +1,7 @@
 output "cloudfront_cdn_domain_name" {
-  value = aws_cloudfront_distribution.cdn.domain_name
+  value = aws_cloudformation_stack.cdn.outputs["CloudFrontCdnDomainName"]
 }
 
 output "cloudfront_cdn_hosted_zone_id" {
-  value = aws_cloudfront_distribution.cdn.hosted_zone_id
+  value = "Z2FDTNDATAQYW2"
 }
