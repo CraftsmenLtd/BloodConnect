9a7e9366c2f486aa9e165e0c358a9bcfa0ff1a1c
Rezwanul-Haque <rezwanul.cse@gmail.com>
2025-02-05 15:38:21 +0600

    I-#415-iac code added

---
+++ iac/terraform/aws/maps/lambdas.tf
@@ -0,0 +1,24 @@
+locals {
+  lambda_options = {
+    autocomplete = {
+      name                       = "get-place-auto-complete"
+      handler                    = "getPlaceAutoComplete.default"
+      zip_path                   = "getPlaceAutoComplete.zip"
+      statement                  = local.policies.common_policies
+      invocation_arn_placeholder = "GET_PLACE_AUTO_COMPLETE_INVOCATION_ARN"
+      env_variables = {
+        GOOGLE_MAP_API_KEY = var.google_map_api_key
+      }
+    },
+    geocode = {
+      name     = "get-geo-code"
+      handler  = "getGeoCode.default"
+      zip_path = "getGeoCode.zip"
+      statement = local.policies.common_policies,
+      invocation_arn_placeholder = "GET_GEO_CODE_INVOCATION_ARN"
+      env_variables = {
+        GOOGLE_MAP_API_KEY = var.google_map_api_key
+      }
+    }
+  }
+}
---
+++ iac/terraform/aws/maps/variables.tf
@@ -0,0 +1,9 @@
+variable "environment" {
+  type        = string
+  description = "Deployment environment"
+}
+
+variable "google_map_api_key" {
+  type        = string
+  description = "google maps api key"
+}
