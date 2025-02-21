8b376584c6121393759f91ada8e0223839f18608
sakibstark11 <16sakib@gmail.com>
2025-02-21 16:37:40 +0000

    fix(430):try fixing pipe missing

--- iac/terraform/aws/eventbridge/eventbridge.tf
+++ iac/terraform/aws/eventbridge/eventbridge.tf
@@ -1,16 +1,11 @@
-resource "null_resource" "check_dynamodb_stream" {
-  depends_on = [var.dynamodb_table_stream_arn]
-
-  provisioner "local-exec" {
-    when    = create
-    command = "aws dynamodb wait table-exists --table-name ${var.dynamodb_table_arn}"
-  }
+data "aws_dynamodb_table" "dynamodb_table" {
+  name = var.dynamodb_table_arn
 }
 
 resource "aws_pipes_pipe" "donation_request_pipe" {
   name     = "${var.environment}-donation-request-pipe"
   role_arn = aws_iam_role.eventbridge_pipe_role.arn
-  source   = var.dynamodb_table_stream_arn
+  source   = data.aws_dynamodb_table.dynamodb_table.stream_arn
   target   = var.donor_search_queue_arn
 
   source_parameters {
@@ -62,15 +57,13 @@ EOF
       log_group_arn = aws_cloudwatch_log_group.donation_request_pipe_log_group.arn
     }
   }
-
-  depends_on = [null_resource.check_dynamodb_stream]
 }
 
 
 resource "aws_pipes_pipe" "donation_accept_pipe" {
   name     = "${var.environment}-donation-accept-pipe"
   role_arn = aws_iam_role.eventbridge_pipe_role.arn
-  source   = var.dynamodb_table_stream_arn
+  source   = data.aws_dynamodb_table.dynamodb_table.stream_arn
   target   = var.donation_status_manager_queue_arn
 
   source_parameters {
@@ -111,8 +104,6 @@ EOF
       log_group_arn = aws_cloudwatch_log_group.donation_accept_pipe_log_group.arn
     }
   }
-
-  depends_on = [null_resource.check_dynamodb_stream]
 }
 
 resource "aws_pipes_pipe" "donation_request_monitoring_pipe" {
