611c7829edec8bc516a1affe200f7c942d45ba75
mahinuzzaman <mahinuzzaman.md@gmail.com>
2025-02-25 15:06:27 +0600

    NO-TICKET: Added old cognito url for now

--- iac/terraform/aws/cognito/local.tf
+++ iac/terraform/aws/cognito/local.tf
@@ -1,4 +1,5 @@
 locals {
-  cognito_domain_prefix = var.environment == module.environments.PRODUCTION ? "signin" : "signin-${replace(replace(replace(replace(var.environment, "aws", ""), "amazon", ""), "cognito", ""), "-", "")}"
-  cognito_domain_name   = "${local.cognito_domain_prefix}.${var.bloodconnect_domain}"
+  cognito_old_domain_name = "bloodconnect-${replace(replace(replace(replace(var.environment, "aws", ""), "amazon", ""), "cognito", ""), "-", "")}"
+  cognito_domain_prefix   = var.environment == module.environments.PRODUCTION ? "signin" : "signin-${replace(replace(replace(replace(var.environment, "aws", ""), "amazon", ""), "cognito", ""), "-", "")}"
+  cognito_domain_name     = "${local.cognito_domain_prefix}.${var.bloodconnect_domain}"
 }
