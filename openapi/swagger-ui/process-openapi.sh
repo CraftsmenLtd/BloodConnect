9ec0e5d0372d82e7b3adc7c55276960799ceb241
mahinuzzaman <mahinuzzaman.md@gmail.com>
2025-03-18 03:15:50 +0600

    NO-TICKET: added swagger ui instructions and update swagger ui scripts

--- openapi/swagger-ui/process-openapi.sh
+++ openapi/swagger-ui/process-openapi.sh
@@ -2,4 +2,8 @@
 
 OPENAPI_FILE="/usr/share/nginx/html/openapi/versions/v1.json"
 sed -i "s/API_GATEWAY_ID/${API_GATEWAY_ID}/g" "$OPENAPI_FILE"
-sed -i "s/BRANCH_NAME/${BRANCH_NAME}/g" "$OPENAPI_FILE"
+if [ "$BRANCH_NAME" == "production" ]; then
+  sed -i "s/BRANCH_NAME//g" "$OPENAPI_FILE"
+else
+  sed -i "s/BRANCH_NAME/${BRANCH_NAME}\./g" "$OPENAPI_FILE"
+fi
