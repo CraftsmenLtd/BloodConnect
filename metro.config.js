3d1b979a153e85e97c57d941bf8cd4bc852b1e33
sakibstark11 <16sakib@gmail.com>
2025-03-26 22:42:25 +0000

    fix(NO-TICKET): fix eslint

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,10 @@
-const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
+import { getDefaultConfig } from 'expo/metro-config';
+import { resolve } from 'path';
 
 // Find the project and workspace directories
 const projectRoot = __dirname;
 // This can be replaced with `find-yarn-workspace-root`
-const monorepoRoot = path.resolve(projectRoot, '../..');
+const monorepoRoot = resolve(projectRoot, '../..');
 
 const config = getDefaultConfig(projectRoot);
 
@@ -12,8 +12,8 @@ const config = getDefaultConfig(projectRoot);
 config.watchFolders = [monorepoRoot];
 // 2. Let Metro know where to resolve packages and in what order
 config.resolver.nodeModulesPaths = [
-  path.resolve(projectRoot, 'node_modules'),
-  path.resolve(monorepoRoot, 'node_modules'),
+  resolve(projectRoot, 'node_modules'),
+  resolve(monorepoRoot, 'node_modules'),
 ];
 
-module.exports = config;
+export default config;
