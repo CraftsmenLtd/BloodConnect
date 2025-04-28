ee5109dd384752ae99a6b38a50de21bcc6f23492
Rezwanul-Haque <rezwanul.cse@gmail.com>
2025-04-28 18:04:11 +0600

    I-#480-mapview empty location api call bug fixed

--- clients/mobile/src/setup/constant/localStorageKeys.ts
+++ clients/mobile/src/setup/constant/localStorageKeys.ts
@@ -1,6 +1,7 @@
 const LOCAL_STORAGE_KEYS = {
   LAST_PROCESSED_NOTIFICATION_KEY: 'lastProcessedNotification',
-  USER_PROFILE: 'userProfile'
+  USER_PROFILE: 'userProfile',
+  LOCATION_COORDINATE_PREFIX: 'locationCoord'
 }
 
 export default LOCAL_STORAGE_KEYS
