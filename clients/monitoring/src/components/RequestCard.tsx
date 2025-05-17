1fcd8e769e9c1009bfd509349d866a97ba997d0d
sakibstark11 <16sakib@gmail.com>
2025-05-17 14:22:25 +0000

    Feat(NO-TICKET): use json view instead

--- clients/monitoring/src/components/RequestCard.tsx
+++ clients/monitoring/src/components/RequestCard.tsx
@@ -1,90 +1,33 @@
-import { Accordion, Table } from 'react-bootstrap'
-import type {
-  CompleteRequest,
-  DynamoDBNumber,
-  NotificationDynamoDBUnmarshaledItem
-} from '../constants/types'
-import { AcceptDonationStatus } from '../../../../commons/dto/DonationDTO'
+import { Accordion } from 'react-bootstrap'
+import JsonView from '@uiw/react-json-view';
+import type { CompleteRequest } from '../constants/types'
+import { vscodeTheme } from '@uiw/react-json-view/vscode';
 
 type RequestCardProps = {
-    data: CompleteRequest;
-    onHeaderClick: (requestId: string) => void;
+  data: CompleteRequest;
+  onHeaderClick: (requestId: string) => void;
 };
 
 const RequestCard = ({ data, onHeaderClick }: RequestCardProps) => {
   const requestId = data.SK.S.split('#')[2]
 
-  const fieldsToIgnore = [
-    'LSI1SK', 'GSI1SK',
-    'PK', 'SK', 'GSI1PK', 'latitude', 'longitude',
-    'patientName', 'shortDescription', 'transportationInfo',
-    'geohash',
-    'notifiedDonors',
-  ]
-
-  const groupedDonorStatuses = data.notifiedDonors?.reduce((acc, item) => {
-    const status = item.status.S;
-    (acc[status as AcceptDonationStatus] ??= []).push(item)
-    return acc
-  }, {} as Partial<Record<AcceptDonationStatus, NotificationDynamoDBUnmarshaledItem[]>>
-  )
-
   return (
     <Accordion className="text-light rounded border-0">
       <Accordion.Item eventKey="0" className="border-0">
         <Accordion.Header className="border-0">{requestId}</Accordion.Header>
-        <Accordion.Body className="bg-dark text-light p-0 border-0" 
-          onEnter={() => onHeaderClick(requestId) }>
-          <Table variant="dark" striped size="sm" className="mb-0">
-            <tbody>
-              {Object.entries(data)
-                .filter(([key]) => !fieldsToIgnore.find(field => field === key))
-                .map(([key, value]) => {
-                  const displayKey = key
-                    .replace(/([A-Z])/g, ' $1')
-                    .replace(/^./, s => s.toUpperCase())
-                  return (
-                    <tr key={key}>
-                      <td><strong>{displayKey}</strong></td>
-                      <td>
-                        {
-                          'S' in value
-                            ? (key === 'createdAt' || key === 'donationDateTime'
-                              ? new Date(value.S).toLocaleString()
-                              : value.S)
-                            : (value as DynamoDBNumber).N
-                        }
-                      </td>
-                    </tr>
-                  )
-                })}
-              {groupedDonorStatuses &&
-                <>
-                  <tr>
-                    <td>Accepted By</td>
-                    <td>{
-                      groupedDonorStatuses[AcceptDonationStatus.ACCEPTED]?.
-                        length}</td>
-                  </tr>
-                  <tr>
-                    <td>Pending For</td>
-                    <td>{groupedDonorStatuses[AcceptDonationStatus.PENDING]?.
-                      length}</td>
-                  </tr>
-                  <tr>
-                    <td>Ignored By</td>
-                    <td>{groupedDonorStatuses[AcceptDonationStatus.IGNORED]?.
-                      length}</td>
-                  </tr>
-                </>
-              }
-
-            </tbody>
-          </Table>
+        <Accordion.Body
+          className="bg-dark text-light p-0 border-0"
+          onEnter={() => onHeaderClick(requestId)}
+        >
+          <JsonView 
+            value={data} 
+            style={{ ...vscodeTheme, overflowX: 'scroll' }}
+            displayDataTypes={false}
+            collapsed={1}
+          />
         </Accordion.Body>
       </Accordion.Item>
     </Accordion>
-
   )
 }
 
--- clients/monitoring/src/constants/constants.ts
+++ clients/monitoring/src/constants/constants.ts
@@ -1,19 +1,3 @@
-export const bloodTypeColors = [
-  '#E57373',
-  '#FF8A65',
-  '#81C784',
-  '#4CAF50',
-  '#64B5F6',
-  '#2196F3',
-  '#FFEB3B',
-  '#FF9800'
-]
-
-export const bloodTypes = [
-  'A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'
-]
-
 export const FIVE_MIN_IN_MS = 5 * 60 * 1000
-
 export const POPUP_CONTROL_CLASS = 'popup-control-class'
 export const MARKER_CONTROL_CLASS = 'marker-control-class'
