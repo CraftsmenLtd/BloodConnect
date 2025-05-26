2e73c6ea5bf592c7014d1aa0ccffa9a687b6b252
Rezwanul-Haque <rezwanul.cse@gmail.com>
2025-05-26 16:34:18 +0600

    I-#437-#BUGFIX: only authenticated pages will try to register the device

--- clients/mobile/src/utility/WithDeviceRegistration.tsx
+++ clients/mobile/src/utility/WithDeviceRegistration.tsx
@@ -1,27 +1,30 @@
 import { useFocusEffect } from '@react-navigation/native'
-import type { ComponentType, FC } from 'react';
+import type { ComponentType, FC } from 'react'
 import { useCallback } from 'react'
 import { InteractionManager } from 'react-native'
+import { useAuth } from '../authentication/context/useAuth'
 import { useFetchClient } from '../setup/clients/useFetchClient'
 import registerUserDeviceForNotification from './deviceRegistration'
 
 export function withRegisterPushOnFocus<P>(WrappedComponent: ComponentType<P>): FC<P> {
   return function ComponentWithPushRegistration(props: P) {
-    const fetchClient = useFetchClient();
+    const fetchClient = useFetchClient()
+    const { isAuthenticated } = useAuth()
 
     useFocusEffect(
       useCallback(() => {
         const task = InteractionManager.runAfterInteractions(() => {
+          if (!isAuthenticated) return
           void registerUserDeviceForNotification(fetchClient).catch(error => {
             // eslint-disable-next-line no-console
-            console.error('Failed to register device:', error);
-          });
+            console.error('Failed to register device:', error)
+          })
         });
 
         return () => task.cancel();
       }, [fetchClient])
-    );
+    )
 
-    return <WrappedComponent {...props} />;
-  };
+    return <WrappedComponent {...props} />
+  }
 }
