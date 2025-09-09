4da7842676ebe41c82f8ffa2e43d5576f03bf0d1
sakibstark11 <16sakib@gmail.com>
2025-09-09 17:28:46 +0000

    test

--- clients/mobile/metro.config.js
+++ clients/mobile/metro.config.js
@@ -1,6 +1,5 @@
 // Learn more https://docs.expo.dev/guides/monorepos
 const { getDefaultConfig } = require('@expo/metro-config');
-const path = require('path');
 
 // See: https://docs.expo.dev/guides/monorepos/#automatic-configuration
 const config = getDefaultConfig(__dirname);
