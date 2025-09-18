91c432cbfc1e320a4086a381fa380acc822600dc
MahinCraftsmen <mahinuzzaman@craftsmensoftware.com>
2025-09-18 16:56:44 +0600

    Add support for React Native 0.81+ property descriptors in metro.config.js

--- clients/mobile/metro.config.js
+++ clients/mobile/metro.config.js
@@ -4,4 +4,8 @@ const { getDefaultConfig } = require('@expo/metro-config');
 // See: https://docs.expo.dev/guides/monorepos/#automatic-configuration
 const config = getDefaultConfig(__dirname);
 
+// Fix for React Native 0.81+ property descriptor issues
+config.resolver.unstable_enablePackageExports = true;
+config.resolver.unstable_conditionNames = ['react-native', 'browser', 'require'];
+
 module.exports = config;
