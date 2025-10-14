75b4afe4b3e69f8ae2091cbf5288507fff601b1a
shoccho <sfb.shoccho@gmail.com>
2025-10-14 16:43:52 +0600

    I-#525 Fix role ARN regex

--- iac/terraform/aws/donor_search/local.tf
+++ iac/terraform/aws/donor_search/local.tf
@@ -15,5 +15,5 @@ locals {
   donor_search_lambda_arn = "arn:aws:lambda:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:function:${local.donor_search_lambda_name}:$LATEST"
 
   eventbridge_scheduler_role_name = "${var.environment}-eventbridge-scheduler-role"
-  eventbridge_scheduler_role_arn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${local.eventbridge_scheduler_role_name}/*"
+  eventbridge_scheduler_role_arn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${local.eventbridge_scheduler_role_name}"
 }
